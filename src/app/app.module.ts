05fbd773f92c8f6a7d1e26fa52d5dbc30819edfb
nguyenkim42 <nguyenkimkm@gmail.com>
2017-08-25 16:24:48 +0700

    Add login-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
