77e8011f0c046d46115982070c3d1f4bb4d2c1fe
trongtao <trongtao.gs@gmail.com>
2017-08-28 16:56:02 +0700

    add Dashboard module & material ui theme

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdTabsModule, MdButtonModule, MdIconModule, MdGridListModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdIconModule,
+    MdGridListModule
+  ],
+  declarations: []
+})
+export class DashboardModule { }
