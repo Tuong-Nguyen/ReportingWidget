f2164a4f30b4bdfcf9202e06637fce911c401b3e
trongtao <trongtao.gs@gmail.com>
2017-08-28 18:28:22 +0700

    rework comments 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Input() isLogin: boolean = false;
-  @Input() userName: string = '';
+  @Input() isLogin = false;
+  @Input() userName = '';
   constructor() { }
 
   ngOnInit() {
   }
-
 }
