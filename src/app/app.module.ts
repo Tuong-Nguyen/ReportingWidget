c8b8feb411895fd83f0354c8349ea675efb67d28
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-30 17:51:58 +0700

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddWidgetModule } from './addwidget/add-widget.module';
 import { UserModule } from './user/user.module';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginFormComponent} from './user/login-form/login-form.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {AddWidgetComponent} from './addwidget/add-widget.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginFormComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'widget', component: AddWidgetComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { UserModule } from './user/user.module';
   imports: [
     BrowserModule,
     AddWidgetModule,
-    UserModule
+    UserModule,
+    DashboardModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
