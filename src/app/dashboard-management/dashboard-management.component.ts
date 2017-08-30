3126aa87d3d968a64af626657de86ed6c545bfb6
trongtao <trongtao.gs@gmail.com>
2017-08-30 11:24:53 +0700

    present mdTable

--- src/app/dashboard-management/dashboard-management.component.ts
+++ src/app/dashboard-management/dashboard-management.component.ts
@@ -6,6 +6,7 @@ import {MdSort} from '@angular/material';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-dashboard-management',
@@ -16,10 +17,22 @@ export class DashboardManagementComponent implements OnInit {
   @Input() dashboards: any;
   displayedColumns = ['dashboardName', 'Actions'];
   dataSource: DashboardDataSource | null;
+  dataSource2: DashboardDataSourceWithoutSort | null;
+  dataSource3: DashboardDataSourceWithoutSortWithoutDataBase | null;
+  dataSource4: DashboardDataSourceWithoutDataBase | null;
 
   @ViewChild(MdSort) sort: MdSort;
   ngOnInit() {
+    const dataArray = [
+      {id: '1', name: 'Aloha'},
+      {id: '2', name: 'Hello'},
+      {id: '2', name: 'Goodluck'},
+      {id: '2', name: 'Xin Chao'},
+      {id: '3', name: 'Goodbye'}];
     this.dataSource = new DashboardDataSource(new DashboardDatabase(this.dashboards), this.sort);
+    this.dataSource2 = new DashboardDataSourceWithoutSort(new DashboardDatabase(dataArray), this.sort);
+    this.dataSource3 = new DashboardDataSourceWithoutSortWithoutDataBase(dataArray, this.sort);
+    this.dataSource4 = new DashboardDataSourceWithoutDataBase(dataArray, this.sort);
     console.log(this.dataSource);
   }
 }
@@ -66,6 +79,82 @@ export class DashboardDataSource extends DataSource<any> {
 
   getSortedData(): Dashboard[] {
     const data = this._exampleDatabase.data.slice();
+    if (!this._sort.active || this._sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'dashboardName':
+          [propertyA, propertyB] = [a.name, b.name];
+          break;
+      }
+
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
+    });
+  }
+}
+export class DashboardDataSourceWithoutSort extends DataSource<any> {
+  private _exampleDatabase: DashboardDatabase;
+  constructor(exampleDatabase: DashboardDatabase, sort: MdSort) {
+    super();
+    this._exampleDatabase = exampleDatabase;
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Dashboard[]> {
+    return this._exampleDatabase.dataChange;
+  }
+
+  disconnect() {}
+
+}
+export class DashboardDataSourceWithoutSortWithoutDataBase extends DataSource<any> {
+  private _exampleDatabase: Dashboard[];
+  constructor(exampleDatabase: Dashboard[], sort: MdSort) {
+    super();
+    this._exampleDatabase = exampleDatabase;
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Dashboard[]> {
+    return Observable.of(this._exampleDatabase);
+  }
+
+  disconnect() {}
+
+}
+export class DashboardDataSourceWithoutDataBase extends DataSource<any> {
+  private _exampleDatabase: Dashboard[];
+  private _sort: MdSort;
+  constructor(exampleDatabase: Dashboard[], sort: MdSort) {
+    super();
+    this._exampleDatabase = exampleDatabase;
+    this._sort = sort;
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Dashboard[]> {
+    const displayDataChanges = [
+      this._exampleDatabase,
+      this._sort.mdSortChange,
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      return this.getSortedData();
+    });
+  }
+
+  disconnect() {}
+
+  getSortedData(): Dashboard[] {
+    const data = this._exampleDatabase.slice();
     if (!this._sort.active || this._sort.direction === '') {
       return data;
     }
