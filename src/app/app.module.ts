511dc05f436dd075000d751000131a6d81776f2e
nguyenkim42 <nguyenkimkm@gmail.com>
2017-08-30 11:52:53 +0700

    add LoginForm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AddWidgetModule } from './addwidget/add-widget.module';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
